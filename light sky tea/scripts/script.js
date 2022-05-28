6c41fbdf38bae6c18333aaedb246db00170071a7
Vendann <93311682+Vendann@users.noreply.github.com>
2022-05-28 10:06:35 +0300

    Update script.js

--- light sky tea/scripts/script.js
+++ light sky tea/scripts/script.js
@@ -2,5 +2,5 @@ let form = document.querySelector('form');
 form.onsubmit = function() {
     var text = document.getElementById("email").value;
     alert('Отправить заявку для ' + text + ' ?');    
-    console.log('Форма отправлена!');
+    alert('Форма отправлена!');
 };
