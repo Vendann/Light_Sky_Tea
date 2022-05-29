6d7341316ba81560673aad5b975f69952629fa2c
Vendann <93311682+Vendann@users.noreply.github.com>
2022-05-29 11:21:39 +0300

    Delete light sky tea directory

--- light sky tea/scripts/script.js
+++
@@ -1,6 +0,0 @@
-let form = document.querySelector('form');
-form.onsubmit = function() {
-    var text = document.getElementById("email").value;
-    alert('Отправить заявку для ' + text + ' ?');    
-    alert('Форма отправлена!');
-};
